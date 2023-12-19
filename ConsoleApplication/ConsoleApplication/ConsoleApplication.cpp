8ae8a3dcfbbcb61eae80ecb89945ee91e2d545c8
MeylandMan <125752061+MeylandMan@users.noreply.github.com>
2023-12-20 00:26:03 +0100

    hello world

--- ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -1,20 +1,12 @@
-// ConsoleApplication.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+#include <iostream> //iostream = in-out stream
 
-#include <iostream>
+//Creating a console is way different from C#
+//std = standard; cout = console? out
+
+//Instead of using dot for referencing a variable in a specific class, we use ::
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Welcome to the console." << std::endl;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
